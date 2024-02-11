21bb803d5d07b3a11e1b874382b339dae054291c
HeshanSnjaya <heshansnjaya@gmail.com>
2024-02-12 02:39:50 +0530

    fixing refresh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import { Navbar, Feed } from "./components";
+import { Navbar, Feed, Videos, Subscribe } from "./components";
 
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Feed />
+      <Routes>
+        <Route path="/new%20videos" exact element={<Videos />} />
+        <Route path="/subscribe" exact element={<Subscribe />} />
+      </Routes>
     </Box>
   </BrowserRouter>
 );
